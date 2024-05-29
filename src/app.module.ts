a2a20fdd82a2bf8962e676cc609c18b14cf2faca
EgydioBNeto <egydiobolonhezi@gmail.com>
2024-05-29 09:58:22 -0300

    Deployando nossa aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql',
-      port: 3306,
-      username: 'admin',
-      password: 'root',
-      database: 'rocketseat-db',
-      entities: [],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'mysql',
+    //   port: 3306,
+    //   username: 'admin',
+    //   password: 'root',
+    //   database: 'rocketseat-db',
+    //   entities: [],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
